c0ee805eecfafc7de6eef293215323199ed42af9
CalebAnderson-Coder <bandersonlopez27.ba@gmail.com>
2024-12-16 16:30:04 -0400

    Fix build configuration and dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
+echo "Setting up environment..."
 export PATH="$PATH:./node_modules/.bin"
 export NODE_OPTIONS="--max-old-space-size=4096"
 
-npm ci
+echo "Installing dependencies..."
+npm install
+
+echo "Building project..."
 ./node_modules/.bin/vite build
